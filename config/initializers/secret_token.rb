9619d7bb2700eced00f7020d9b5b068068f37b64
michaelward82 <m.ward@leythers.com>
2013-12-27 22:33:24 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ttcliche::Application.config.secret_key_base = '08a25baeb3f5beab72365567acb40ac5730468c2914ac71c4c1a2fecbe3aa122648cfa0551720cdf883e42054596803d2b5a56db7969b4338ea579250398d83b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ttcliche::Application.config.session_store :cookie_store, key: '_ttcliche_session'
